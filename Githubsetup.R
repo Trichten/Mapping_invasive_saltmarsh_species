e7445e42463aea5b0cad43acb9faa2af48edf2a6
Trichten <timrichter97@gmx.de>
2021-11-16 08:27:59 +0100

    Getting started

---
+++ Githubsetup.R
@@ -0,0 +1,11 @@
+library(usethis)
+use_git_config(user.name="Trichten", user.mail="timrichter97@gmx.de")
+
+#oder in der Shell
+#C:\Users\Dana>git config --global user.email "EMAIL"
+#C:\Users\Dana>git config --global user.name "NAME"
+
+
+
+
+
